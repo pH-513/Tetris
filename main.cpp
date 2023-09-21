55289fc67d9f1b84f92702639d90bdfaa024ff26
조영현 <133370084+choyeounghyeon@users.noreply.github.com>
2023-09-21 19:46:49 +0900

    2023.09.21 뉴 커밋
    Block 클래스와 SetBlock 클래스:
    기존 Block 클래스와 SetBlock 클래스의 내용을 합쳐서 하나의 SetBlock 클래스로 통합했습니다. 이렇게 하면 블록과 관련된 모든 기능을 하나의 클래스에서 처리할 수 있습니다.
    
    Setlayout 클래스:
    Setlayout 클래스는 그대로 유지되었습니다. 배경 레이아웃 출력과 관련된 기능을 처리합니다.
    
    load 함수:
    load 함수에서 클래스 인스턴스를 생성할 때 SetBlock 대신 GameBlocks를 사용하도록 수정했습니다. 클래스의 멤버 함수 호출에서도 괄호를 추가하여 호출하도록 수정했습니다.
    
    GameStart 함수:
    GameStart 함수도 거의 그대로 유지되었으며, 클래스 인스턴스 생성 및 함수 호출 시 괄호를 추가하여 호출하도록 수정했습니다.
    
    main 함수:
    main 함수에서는 더 이상 SetBlock 클래스의 인스턴스를 생성하지 않고, GameStart 함수만 호출하여 게임을 시작합니다.

--- main.cpp
+++ main.cpp
@@ -1,58 +1,57 @@
 #include <iostream>
 #include <ctime>
-#include <Windows.h>
-#include <conio.h> // getch 함수를 사용하기 위한 헤더
+#include <Windows.h> //Sleep()
+#include <conio.h> // getch()
 #include <stdlib.h>
 #include <array>
-
 using namespace std;
 
-class Block {
+class SetBlock {
 private:
-    int blocks[7][4][4] = {
-        // I 블록
+    int SetBlocks[7][4][4] = {
+        // I 블록 1
         {
             {8, 8, 8, 8},
             {1, 1, 1, 1},
             {8, 8, 8, 8},
             {8, 8, 8, 8}
         },
-        // J 블록
+        // J 블록 2
         {
             {8, 8, 8, 8},
             {8, 1, 1, 1},
             {8, 1, 8, 8},
             {8, 8, 8, 8}
         },
-        // L 블록
+        // L 블록 3
         {
             {8, 8, 8, 8},
             {8, 1, 1, 1},
             {8, 8, 8, 1},
             {8, 8, 8, 8}
         },
-        // O 블록
+        // O 블록 4
         {
             {8, 8, 8, 8},
             {8, 1, 1, 8},
             {8, 1, 1, 8},
             {8, 8, 8, 8}
         },
-        // S 블록
+        // S 블록 5
         {
             {8, 8, 8, 8},
             {8, 8, 1, 1},
             {8, 1, 1, 8},
             {8, 8, 8, 8}
         },
-        // T 블록
+        // T 블록 6
         {
             {8, 8, 8, 8},
             {8, 1, 1, 1},
             {8, 8, 1, 8},
             {8, 8, 8, 8}
         },
-        // Z 블록
+        // Z 블록 7
         {
             {8, 8, 8, 8},
             {1, 1, 8, 8},
@@ -96,14 +95,14 @@ public:
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 // 현재 블록의 값을 임시 배열에 복사
-                temp[i][j] = blocks[y][i][j];
+                temp[i][j] = SetBlocks[y][i][j];
             }
         }
 
         // 90도 회전 로직
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                blocks[y][i][j] = temp[3 - j][i];
+                SetBlocks[y][i][j] = temp[3 - j][i];
             }
         }
 
@@ -112,13 +111,8 @@ public:
     }
 };
 
-int main() {
-
-    /**************************
-    *           배경          *
-    **************************/
-
-
+class Setlayout {
+private:
     int layout[20][10] = {
         {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
         {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -140,38 +134,105 @@ int main() {
         {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
         {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
     };
+public:
+    void printlayout() {
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (layout[i][j] == 1) {
+                    // 1인 경우 출력할 문자 (예: 'X') 사용
+                    std::cout << 'X';
+                }
+                else {
+                    // 0인 경우 출력할 문자 (예: '.') 사용
+                    std::cout << '.';
+                }
+            }
+            std::cout << std::endl; // 행이 끝날 때 줄 바꿈
+        }
+    }
+};
 
-    Block block; // Block 클래스의 인스턴스 생성
+void load(const Setlayout& gameLayout, const SetBlock& GameBlocks) {
+    gameLayout.printlayout(); // 함수 호출에 괄호 추가
+    int getX = GameBlocks.getX(); // 함수 호출에 괄호 추가
+    int getY = GameBlocks.getY(); // 함수 호출에 괄호 추가
+    int getR = GameBlocks.getR(); // 함수 호출에 괄호 추가
+    cout << "X값 : " << getX << endl;
+    cout << "Y값 : " << getY << endl;
+    cout << "회전값 : " << getR << endl;
+}
+
+void GameStart() {
+    Setlayout gameLayout;
+    gameLayout.printlayout(); // GameLayout 출력
+
+    int loop = 0;
 
     while (true) {
+        SetBlock GameBlocks;
+        GameBlocks.setX(4);
+        load;
+
+        // 일정 시간 동안 대기 (1초)
+        Sleep(1000);
+
+        int key = _getch();
+        switch (key) {
+        case 77: // RIGHT (→ 키)
+            GameBlocks.setX(GameBlocks.getX() + 1);
+            load;
+            break;
+        case 75: // LEFT (← 키)
+            GameBlocks.setX(GameBlocks.getX() - 1);
+            load;
+            break;
+        }
+        
+        // 다음 블록 생성
+        SetBlock NextBlock;
+        NextBlock.setY(loop);
+
+        // 루프 종료 조건 추가 (예: 게임 종료 조건)
+        if (loop >= 10 //임시
+            ) {
+            break;
+        }
+
+        loop++;
+    }
+}
+
+int main() {
+    SetBlock SetBlock; // SetBlock 클래스의 인스턴스 생성
+    GameStart();
+    return 0;   // 프로그램 종료
+}
+
+/* while (true) {
+        SetBlock GameBlocks;
+        GameBlocks.setX(4);
+
         // 키 입력 처리
         int key = _getch();
         switch (key) {
         case 'a': // 'a' 키를 누르면 왼쪽으로 이동
-            block.setX(block.getX() - 1);
+            SetBlock.setX(SetBlock.getX() - 1);
             break;
         case 'd': // 'd' 키를 누르면 오른쪽으로 이동
-            block.setX(block.getX() + 1);
+            SetBlock.setX(SetBlock.getX() + 1);
             break;
         case 's': // 's' 키를 누르면 아래로 이동
-            block.setY(block.getY() + 1);
+            SetBlock.setY(SetBlock.getY() + 1);
             break;
         case 'w': // 'w' 키를 누르면 블록 회전
-            block.rotate();
+            SetBlock.rotate();
             break;
         }
-        
-        system("cls");
 
-        int getX = block.getX();
-        int getY = block.getY();
-        int getR = block.getR();
+        system("cls");
+        int getX = GameBlocks.getX();
+        int getY = GameBlocks.getY();
+        int getR = GameBlocks.getR();
         cout << "X값 : " << getX << endl;
         cout << "Y값 : " << getY << endl;
-        cout << "회전값 : " << getR << endl;
-
-        //         Logic
-
-    }
-    return 0;   // 프로그램 종료
-}
+        cout << "회전값 : " << getR << endl;*/
